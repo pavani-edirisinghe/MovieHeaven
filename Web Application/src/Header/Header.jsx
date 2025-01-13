ec15bea6d89815e86f8c9df1366ddce4e904905f
pavani-edirisinghe <pavaniedirisinghe18@gmail.com>
2025-01-13 10:46:25 +0530

    Create Header

---
+++ Web Application/src/Header/Header.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import "./Header.css";
+
+function Header() {
+  const [isActive, setIsActive] = useState(false);
+
+  // const toggleMenu = () => {
+  //   setIsActive(!isActive);
+  // };
+
+  return (
+    <header id="header" className="fixed-top header-inner-pages">
+      <div className="container d-flex align-items-center justify-content-between">
+    
+          <img src="movieHeaven.png" alt="Logo" width="60" height="60" padding-left= "30px" />
+        <nav id="navbar" className="navbar">
+          <ul>
+            <li><a className="nav-link scrollto" href="index.html">Home</a></li>
+            <li><a className="nav-link scrollto" href="index.html">About Us</a></li>
+            <li><a className="nav-link scrollto" href="index.html">Movies</a></li>
+            <li><a className="nav-link scrollto" href="index.html">Sign In</a></li>
+            <li><a className="nav-link scrollto" href="index.html">Contact Us</a></li>
+          </ul>
+          {/* <i className="bi bi-list mobile-nav-toggle" onClick={toggleMenu}></i> */}
+        </nav>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
